e6d99720dcb530d90355dd483feca76312f5ac99
cbarksdale2321 <55416957+cbarksdale2321@users.noreply.github.com>
2020-12-09 10:07:37 -0600

    Added Sorting Assignment

---
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HeapSort
+            int[] arr = { 12, 11, 13, 5, 6, 7 };
+            int n = arr.Length;
+            Program ob = new Program();
+            ob.sort(arr);
+
+            Console.WriteLine("HeapSorted array is");
+            printArray(arr);
+            //end HeapSort
+
+            //Quick Sort
+     
+            quickSort(arr, 0, n - 1);
+            Console.WriteLine("QuickSorted array ");
+            printArray(arr);
+        }
+
+
+        public void sort(int[] arr)
+        {
+            int n = arr.Length;
+
+            // Build heap (rearrange array)
+            for (int i = n / 2 - 1; i >= 0; i--)
+                heapify(arr, n, i);
+
+            // One by one extract an element from heap
+            for (int i = n - 1; i > 0; i--)
+            {
+                // Move current root to end
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                // call max heapify on the reduced heap
+                heapify(arr, i, 0);
+            }
+        }
+
+        void heapify(int[] arr, int n, int i)
+        {
+            int largest = i; 
+            int l = 2 * i + 1; 
+            int r = 2 * i + 2; 
+
+           
+            if (l < n && arr[l] > arr[largest])
+                largest = l;
+
+            if (r < n && arr[r] > arr[largest])
+                largest = r;
+
+            
+            if (largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+
+                
+                heapify(arr, n, largest);
+            }
+        }
+
+        
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.WriteLine(arr[i] + " ");
+            Console.ReadLine();
+        }
+
+        static int partition(int[] arr, int low,
+                                  int high)
+        {
+            int pivot = arr[high];
+
+            // index of smaller element 
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+                // If current element is smaller  
+                // than the pivot 
+                if (arr[j] < pivot)
+                {
+                    i++;
+
+                    // swap arr[i] and arr[j] 
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            // swap arr[i+1] and arr[high] (or pivot) 
+            int temp1 = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp1;
+
+            return i + 1;
+        }
+        static void quickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = partition(arr, low, high);
+
+                quickSort(arr, low, pi - 1);
+                quickSort(arr, pi + 1, high);
+            }
+        }
+    }
+
+
+}
